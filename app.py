d6e2b6bf5796449cb36b52caa99fdd9de10724c4
Muthu Kumar <muthukumarmk2306@gmail.com>
2026-07-02 13:45:57 +0530

    Add Gmail unread email reader with accurate received time

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from gmail.auth import authenticate
+from gmail.gmail_reader import get_unread_emails
+
 
 def main():
-    authenticate()
+
+    print("\n🚀 Starting SmartMail Notify...\n")
+
+    creds = authenticate()
+
     print("✅ Gmail Authentication Successful!")
+
+    get_unread_emails(creds)
+
 
 if __name__ == "__main__":
     main()
