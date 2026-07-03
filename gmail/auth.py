f90d80e6f53ab4631cf194f2a38aa9f85f2f79bf
Muthu Kumar <muthukumarmk2306@gmail.com>
2026-07-04 02:36:59 +0530

    Load Gmail credentials from Railway variables

--- gmail/auth.py
+++ gmail/auth.py
@@ -2,11 +2,28 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 import os
-
+import json
 # Permission to read Gmail
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+def create_secret_files():
+    """
+    Create credentials.json and token.json from Railway variables
+    if they do not already exist.
+    """
+
+    credentials = os.getenv("CREDENTIALS_JSON")
+    token = os.getenv("TOKEN_JSON")
+
+    if credentials and not os.path.exists("credentials.json"):
+        with open("credentials.json", "w") as f:
+            f.write(credentials)
+
+    if token and not os.path.exists("token.json"):
+        with open("token.json", "w") as f:
+            f.write(token)
 
 def authenticate():
+    create_secret_files()
     creds = None
 
     # Load existing token if available
